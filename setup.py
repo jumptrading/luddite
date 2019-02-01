b0a5e0967ccacd30d1d648426b2ddb3a53e47122
Wim Glenn <wglenn@jumptrading.com>
2019-02-01 17:41:17 -0600

    support terminal color on windows

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="luddite",
-    version="1.0.0",
+    version="1.0.1",
     author="Wim Glenn",
     author_email="hey@wimglenn.com",
     url="https://github.com/jumptrading/luddite",
@@ -17,10 +17,13 @@ setup(
         "Topic :: Utilities",
     ],
     entry_points={"console_scripts": ["luddite=luddite:main"]},
-    install_requires=["setuptools>=18.0"],
+    install_requires=[
+        "setuptools >= 18.0",
+        'colorama; platform_system == "Windows"',
+        'futures; python_version < "3.2"',
+    ],
     extras_require={
         # https://hynek.me/articles/conditional-python-dependencies/
-        ":python_version<'3.2'": ["futures"],
         "dev": [
             "pytest>=3.6.3",
             "pytest-cov",
